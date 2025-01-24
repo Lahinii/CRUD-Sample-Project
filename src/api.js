f1f7526256c75134f4a1676988cec00a0fe61434
Lahini Balasooriya <92258335+Lahinii@users.noreply.github.com>
2025-01-24 07:24:17 +0000

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,3 +7,5 @@ export const getProductById = async (id) => axios.get(`${API_URL}/${id}`);
 export const createProduct = async (product) => axios.post(API_URL, product);
 export const updateProduct = async (id, product) => axios.put(`${API_URL}/${id}`, product);
 export const deleteProduct = async (id) => axios.delete(`${API_URL}/${id}`);
+export const getCategories = async () => axios.get(`${API_URL}/categories`);
+export const createCategory = async (category) => axios.post(`${API_URL}/categories`, category);
